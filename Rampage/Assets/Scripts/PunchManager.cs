29c9fa4b84a427612dc497290dda052b637b15ad
Ian Reimers <ian.reimers.apps@gmail.com>
2023-03-15 18:48:17 -0600

    modified punch script to dynamically retreive a punch manager to trim script variable dependencies

--- Rampage/Assets/Scripts/PunchManager.cs
+++ Rampage/Assets/Scripts/PunchManager.cs
@@ -9,40 +9,47 @@ public class PunchManager : MonoBehaviour
   public Transform hand;
   public InputActionProperty primaryButton;
   public Collider punchCollider;
-  public ParticleManager particleManager;
-  private bool canPunch;
-  private Vector3 handsDir;
+  private ParticleManager _particleManager;
+  private bool _canPunch;
+  private Vector3 _handsDir;
+  void Awake()
+  {
+    transform.position = hand.position; // Start at rig position
+    rb = GetComponent<Rigidbody>();
+    _canPunch = false;
+    _particleManager = GameObject.FindWithTag("ParticleManager").GetComponent<ParticleManager>();
+
+    // Give error if a Particle Manager is not initialized
+    if (_particleManager == null)
+    {
+      Debug.LogError("Couldn't find particle manager script on particle manager object");
+    }
+
+  }
   // Start is called before the first frame update
   void Start()
   {
-    // Start at rig position
-    transform.position = hand.position;
-
-    canPunch = false;
-    rb = GetComponent<Rigidbody>();
   }
 
   void Update()
   {
 
-    handsDir = hand.position - rb.position;
-    // primaryButton.action.ReadValue<float>() == 1
+    _handsDir = hand.position - rb.position;
     if (primaryButton.action.ReadValue<float>() == 1)
     {
-      canPunch = true;
+      _canPunch = true;
     }
     else
     {
-      canPunch = false;
+      _canPunch = false;
     }
   }
 
   // Update is called once per frame
   void FixedUpdate()
   {
-    //Moves physics hands to controller
+    //Moves physics hands to controller with controller rotation
     rb.MovePosition(hand.transform.position);
-    // rb.velocity = handsDir / Time.fixedDeltaTime;
     rb.MoveRotation(hand.rotation);
   }
 
@@ -55,25 +62,25 @@ public class PunchManager : MonoBehaviour
 
   }
 
-  void OnCollisionEnter(Collision other)
+  void OnCollisionEnter(Collision collider)
   {
 
-    if (canPunch)
+    if (_canPunch)
     {
       // if (collision.transform.tag != "WallChunk") { return; }
-      Vector3 forceOfHit = other.impulse / Time.fixedDeltaTime;
+      Vector3 forceOfHit = collider.impulse / Time.fixedDeltaTime;
       Vector3 clampedForce = Vector3.ClampMagnitude(forceOfHit, 100);
-      var contact = other.GetContact(0);
-      // print("Unclamped Force: " + forceOfHit + ", Clamped Force: " + clampedForce);
+      ContactPoint contact = collider.GetContact(0);
 
-      other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(clampedForce * 25, ForceMode.Force);
+      // print("Unclamped Force: " + forceOfHit + ", Clamped Force: " + clampedForce);
+      collider.gameObject.GetComponent<Rigidbody>().AddRelativeForce(clampedForce * 25, ForceMode.Force);
       // collision.gameObject.GetComponent<Rigidbody>().AddRelativeForce(final * 50, ForceMode.Force);
 
       // Spawn a hit particle
-      particleManager.SpawnPunchParticles(other.GetContact(0).point, contact.normal);
+      _particleManager.SpawnPunchParticles(contact.point, contact.normal);
 
       // Joint damage to the WallChunk
-      ConfigurableJoint[] joints = other.gameObject.GetComponents<ConfigurableJoint>();
+      ConfigurableJoint[] joints = collider.gameObject.GetComponents<ConfigurableJoint>();
 
       if (joints.Length == 0) { return; }
 
@@ -85,7 +92,7 @@ public class PunchManager : MonoBehaviour
     }
     else
     {
-      if (other.transform.tag == "WallChunk")
+      if (collider.transform.tag == "WallChunk")
       {
         punchCollider.isTrigger = true;
         // WallChunk wallChunk = other.gameObject.GetComponent<WallChunk>();
