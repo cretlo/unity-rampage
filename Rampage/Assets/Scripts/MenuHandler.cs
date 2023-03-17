52e76ea935caae677b1c294148181ef941444d05
Ian Reimers <ian.reimers.apps@gmail.com>
2023-03-16 21:25:40 -0600

    added canvas menu prefab along with menu scale learping logic

---
+++ Rampage/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuHandler : MonoBehaviour
+{
+  public InputActionProperty joystickButton;
+  private RectTransform _canvas;
+  private float _canvasWidth;
+  private float _canvasHeight;
+  private bool _joystickClickActive;
+  private float _time;
+  private float maxScale = 0.1f;
+
+  private float lerpedScale;
+  private float lerpedShrinkingScale;
+  void Awake()
+  {
+    _canvas = GetComponent<RectTransform>();
+    _canvasWidth = _canvas.localScale.x;
+    _canvasHeight = _canvas.localScale.y;
+    _joystickClickActive = false;
+    _time = 0;
+
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+
+
+    // Set initial size
+    _canvas.localScale = Vector3.zero;
+
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ShowMenu();
+
+  }
+
+  void ShowMenu()
+  {
+    _joystickClickActive = joystickButton.action.ReadValue<float>() == 1;
+
+    if (_joystickClickActive)
+    {
+      if (_time >= 0.2)
+      {
+        return;
+      }
+      lerpedScale = Mathf.Lerp(0, 0.01f, _time / 0.2f);
+      _canvas.localScale = new Vector3(lerpedScale, lerpedScale, 0.01f);
+      _time += Time.deltaTime;
+
+    }
+    else
+    {
+      if (_time <= 0)
+      {
+        return;
+      }
+
+      lerpedShrinkingScale = Mathf.Lerp(0, _canvas.localScale.x, _time * 2f);
+      _canvas.localScale = new Vector3(lerpedShrinkingScale, lerpedShrinkingScale, 0.01f);
+      _time -= Time.deltaTime;
+
+    }
+
+  }
+}
